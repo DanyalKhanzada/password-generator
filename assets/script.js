94af19de4ddb45f11655484ecf56e3b47e4a08e9
“Danyal <danyalkhanzada89@gmail.com>
2021-03-14 17:06:12 -0400

    global-variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
-// Assignment code here
+// * Global Variables
+var lowerCaseInput = false;
+var upperCaseInput = false;
+var numberInput = false;
+var specialCharInput = false;
+var passLength = '0';
+var passLengthInt = 0;
+
+let lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specialCharArray = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
 
 
 // Get references to the #generate element
