6284d09ba098ce6914c2039823121729e6adaf9e
“Danyal <danyalkhanzada89@gmail.com>
2021-03-14 18:16:20 -0400

    Click-button-added

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,30 @@ let upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
 let numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let specialCharArray = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
 
+// Click Button
+  function charSelect(){  
+
+    //confirm box for lowercase letters - OK to add, cancel to leave out
+    lowerCaseInput = confirm("Would you like lower case letters?");
+  
+    //confirm box for uppercase letters - OK to add, cancel to leave out
+      upperCaseInput = confirm("Would you like upper case letters?");
+  
+    //confirm box for numbers - OK to add, cancel to leave out 
+      numberInput = confirm("Would you like numbers?");
+  
+    //confirm box for special char - OK to add, cancel to leave out
+      specialCharInput = confirm("Would you like special characters?");
+  
+    //Validate and choose at least one of each selected criteria
+      if( lowerCaseInput == true || upperCaseInput == true || numberInput == true || specialCharInput == true){
+        alert("Thanks for your input. Engaging code tumblers.")
+      }
+      else{
+        alert("Please choose at least one character type.");
+        charSelect();
+      }; 
+  };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
